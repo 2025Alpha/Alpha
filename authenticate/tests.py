e1e640a19f991ca93150024a22c65accc7d7c334
jimmy914 <jimmy7620@naver.com>
2025-03-31 15:13:22 +0900

    login

--- authenticate/tests.py
+++ authenticate/tests.py
@@ -1,7 +1,13 @@
 from django.test import TestCase
-from config.settings import (KAKAO_AUTH_CODE, # 임시 인가 코드를 가져옵니다. 테스트 실행시마다 .env 파일에서 매번 바꿔줘야합니다.
-                             KAKAO_REFRESH_TOKEN # 리프레시 토큰. 만료시 바꿔 사용
-                             )
+from config.settings import (
+    KAKAO_AUTH_CODE, # 임시 인가 코드를 가져옵니다. 테스트 실행시마다 .env 파일에서 매번 바꿔줘야합니다.
+    KAKAO_REFRESH_TOKEN, # 리프레시 토큰. 만료시 바꿔 사용
+    KAKAO_TEST_ACCESS_TOKEN,
+    KAKAO_TEST_ID_TOKEN,
+    APP_LOGGER
+)
+import logging
+logger = logging.getLogger(APP_LOGGER)
 
 # Create your tests here.
 
@@ -12,9 +18,9 @@ class TestAuthenticate(TestCase):
         해당 테스트는 카카오 로그인 콜백이 정상적으로 이루어지는지 확인하기 위한 코드입니다.
         """
         redirect_uri = 'http://localhost:8000/auth/login/'
-        response = self.client.post(f'/auth/login/?code={self.AUTH_CODE}&redirect_uri={redirect_uri}')
-        print(response.json())
+        response = self.client.post(f'/auth/get_token/?code={self.AUTH_CODE}&redirect_uri={redirect_uri}')
         self.assertEqual(response.status_code, 201)
+        print(response.json())
 
     def test_refresh_token(self):
         """
@@ -27,7 +33,6 @@ class TestAuthenticate(TestCase):
         }
         response = self.client.post(target_uri, data=data, content_type='application/json')
         self.assertEqual(response.status_code, 201)
-        print(response.json())
         # 리프레시 토큰 정보를 body에서 발견하지 못한 경우를 테스트 합니다.
         data = {
             # 일부러 틀린 정보를 넣습니다.
@@ -41,3 +46,32 @@ class TestAuthenticate(TestCase):
         }
         response = self.client.post(target_uri, data=data, content_type='application/json')
         self.assertEqual(response.status_code, 400)
+
+    def test_login(self):
+        """
+        해당 함수는 flutter sdk로 발급받은 액세스 토큰과 아이디 토큰을 활용하여 로그인 혹은 회원가입 진행이 되는지 확인합니다.
+        """
+        end_point = '/auth/login/'
+        headers = {
+            'Authorization': f'Bearer {KAKAO_TEST_ACCESS_TOKEN}'
+        }
+        data = {
+            'id_token': KAKAO_TEST_ID_TOKEN,
+        }
+        # register Test
+        response = self.client.post(end_point, headers=headers, data=data, content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json()['is_new'], True)
+
+        # login Test
+        response = self.client.post(end_point, headers=headers, data=data, content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json()['is_new'], False)
+
+        # 400 Test
+        data2 = {
+            'id_token' : 'hsesefs'
+        }
+        response = self.client.post(end_point, headers=headers, data=data2, content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
